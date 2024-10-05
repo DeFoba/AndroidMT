3a43b6e6f33d1122109cbc2c34a3e1b64002b284
DeFoba <51561763+DeFoba@users.noreply.github.com>
2024-10-05 15:51:02 +0300

    Add entry field & Create true list

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -10,12 +10,25 @@ export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
   constructor() {}
-
+  new_item = ''
+  public appPages = [
+    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
+    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
+    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
+    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
+    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
+    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+  ];
+  public l_ist = ['Test 1', 'Test 2', 'Test 3', 'Test 4', 'Test 5']
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
   }
 
   btn_click() {
     console.log(this);
+  }
+
+  add_in_list() {
+    this.l_ist.push(this.new_item)
   }
 }
